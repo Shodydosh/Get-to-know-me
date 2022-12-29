2b27690f79baf293d4934d8adab6bc63698949db
Tung Nguyen Minh <nguyenminhtung252@gmail.com>
2022-12-29 18:31:21 +0700

    Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./assets/scss/app.scss";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
